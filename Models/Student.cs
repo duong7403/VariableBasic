41bbeb68778949c037520efaa4a8ec32e16b8b5f
Duong <trinhduong0216@gmail.com>
2025-01-24 10:46:50 +0700

    Implement

--- Models/Student.cs
+++ Models/Student.cs
@@ -4,5 +4,8 @@ namespace VariableBasic;
 
 public class Student
 {
-
+    public int id { get; set; }
+        
+    public string Name { get; set; }
+    public string Email { get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using VariableBasic;
+
 Console.WriteLine("Hello, .Net02!");
+
+var Cong = new Student();
+Cong.id = 1;
+Cong.Name = "AAAA";
+Cong.Email = "ASDA@SDASDAS";
